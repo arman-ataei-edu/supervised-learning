70a8224582e6aabf36223610185293fda67aa0e9
arman-ataei <arman.lovelywebdev@gmail.com>
2024-12-19 17:48:37 +0330

    R^2 score

--- 3-classification/KNN/KNN.py
+++ 3-classification/KNN/KNN.py
@@ -2,11 +2,21 @@ import numpy as np
 from collections import Counter
 from .Metrics import Metric, Metrics
 class KNN:
-    def __init__(self,distance_metric:Metrics, k:int=3,weighted=False, **kwargs):
+    def __init__(self,
+                 distance_metric:Metrics, 
+                 k:int=3,
+                 weighted=False, 
+                 centeralized=False, 
+                 **kwargs):
+        
         assert k >0 and k%2>0
-        self.p = kwargs['p']
-        self.k = k
+        # currently we dont compute the centralized weighted KNN
+        assert not weighted or not centeralized
+
+        self.p = kwargs['p'] if kwargs and kwargs['p'] else None
+        self.k = k 
         self.weighted = weighted
+        self.centeralized = centeralized
         if distance_metric == Metrics.Lp:
             if not self.p:
                 return ValueError("please set the value for 'p'")
@@ -22,6 +32,28 @@ class KNN:
 
         self.X_train = X_train
         self.y_train = y_train
+        assert self.k < len(X_train)
+        # computing the centers of classes(mean)
+        
+
+        if self.centeralized:
+            labels = set(self.y_train)
+            leaders = []
+            assert self.k <= len(labels)
+            l = len(self.X_train)
+
+            for label in labels:
+                mean = np.zeros(shape=X_train[0].shape)
+                # number of elements in the "label" class
+                n_class = 0
+                for i in range(l):
+                    if y_train[i] == label:    
+                        mean += X_train[i]
+                        n_class +=1
+                # print(mean)
+                leaders.append((label, mean/n_class, n_class))
+        
+            self.leaders = leaders
 
     def predict(self, X_test:np.ndarray):
         # assert X_test.shape[0] == self.train_shape
@@ -40,6 +72,10 @@ class KNN:
         k_indices = np.argsort(distances)[:self.k]
         k_nearest_labels = [self.y_train[i] for i in k_indices]
        
+        # Calculating centralized weighted vote
+        if self.centeralized and self.weighted:
+            pass
+
         # Calculating the weighted vote
         if self.weighted:
             k_nearest_distances = [distances[i] for i in  k_indices]
@@ -53,7 +89,27 @@ class KNN:
             # print(weighted_votes)
             return weighted_votes.most_common(1)[0][0]
         
+        # calculating the votes w.r.t center of classes
+        if self.centeralized:
+            if self.p :
+                distances = [self.metric.calc(x, leader[1], self.p) for leader in self.leaders]
+            if not self.p:
+                distances = [self.metric.calc(x, leader[1]) for leader in self.leaders]
+            
+            # Get the k nearest samples, their labels
+            k_indices = np.argsort(distances)[:self.k]
+            return self.leaders[k_indices[-1]][0]
+            # k_nearest_labels = [self.leaders[i][0] for i in k_indices]
+
         # Return the most common class label
         most_common = Counter(k_nearest_labels).most_common(1)
         return most_common[0][0]
 
+    def score(self, y_test, y_pred):
+        """
+        the R² score
+        R^2 = 1 - \frac{\sum (y_i - \hat{y}_i)^2}{\sum (y_i - \bar{y})^2}
+        """
+        ss_total = np.sum((y_test - np.mean(y_test)) ** 2) 
+        ss_residual = np.sum((y_test - y_pred) ** 2) 
+        return 1 - (ss_residual / ss_total)
